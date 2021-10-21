69e63b521b7a488d345662819b6ae024281e7505
nnhi9428 <nnhi9428@gmail.com>
2021-10-21 15:42:35 +0700

    Add project files.

---
+++ TechnologySystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechnologySystem.MvcApplication" Language="C#" %>
---
+++ TechnologySystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechnologySystem.Startup))]
+namespace TechnologySystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
