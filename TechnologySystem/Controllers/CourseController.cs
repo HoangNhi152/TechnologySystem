bc710b941d97af1c4757808b4dd7e1242d187c7a
nnhi9428 <nnhi9428@gmail.com>
2021-10-21 23:45:14 +0700

    Add Index functionality for Course

---
+++ TechnologySystem/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechnologySystem.Models;
+
+namespace TechnologySystem.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CourseController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index(string searchString)
+        {
+            var courses = _context.Courses.ToList();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToLower();
+                courses = courses.Where(c => c.CourseName.ToLower().Contains(searchString)).ToList();
+            }
+
+            return View(courses);
+        }
+
+    }
+}
