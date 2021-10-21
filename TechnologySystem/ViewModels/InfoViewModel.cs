786f25ea68c03f5ff09303bb92fdf02e8bef35e3
nnhi9428 <nnhi9428@gmail.com>
2021-10-21 17:28:18 +0700

    Add Edit functonality (GET_POST) for Admin

---
+++ TechnologySystem/ViewModels/InfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TechnologySystem.Models;
+
+namespace TechnologySystem.ViewModels
+{
+    public class InfoViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public List<string> Roles { get; set; }
+        public string Specialty { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime BirthDate { get; set; }
+
+        public string Education { get; set; }
+    }
+}
