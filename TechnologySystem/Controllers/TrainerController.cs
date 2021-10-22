9ac5496cdba7ee6a909211254093a1286e24f2c2
congprodh <congprodh@gmail.com>
2021-10-22 16:56:41 +0700

    Add Index functionality for Trainer

---
+++ TechnologySystem/Controllers/TrainerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechnologySystem.Models;
+
+namespace TechnologySystem.Controllers
+{
+	public class TrainerController : Controller
+	{
+		// GET: Trainer
+		private ApplicationDbContext _context;
+		public TrainerController()
+		{
+			_context = new ApplicationDbContext();
+		}
+		// GET: TrainerInfo
+		public ActionResult Index()
+		{
+			var userId = User.Identity.GetUserId();
+			var trainer = _context.Users.SingleOrDefault(u => u.Id.Equals(userId));
+
+			if (trainer == null) return HttpNotFound();
+			return View(trainer);
+		}
+	}
+}
